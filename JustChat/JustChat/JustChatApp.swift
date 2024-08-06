29a620bbf9741b56c14e210ddb77458001f98e07
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-08-06 14:33:17 +0700

    add project

---
+++ JustChat/JustChat/JustChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustChatApp.swift
+//  JustChat
+//
+//  Created by Василий Клецкин on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct JustChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
