cde3e1583b44ba97d73607108150fc4fdb20ec4e
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-08-19 10:07:33 +0700

    remote request creation for register screen

--- JustChat/JustChat/Authentication/Register/API/Register+RequestCreation.swift
+++ JustChat/JustChat/Authentication/Register/API/Register+RequestCreation.swift
@@ -9,3 +9,12 @@ struct RegisterRequestDTO: Encodable {
     let email: String
     let username: String
 }
+
+extension RegisterRequest {
+    func remoteRequest(with encode: RemoteEncoder<RegisterRequestDTO>) -> RemoteRequest {
+        let path = "register"
+        let method = "POST"
+        let body = encode(RegisterRequestDTO(email: email, username: username))
+        return .init(path: path, method: method, body: body)
+    }
+}
