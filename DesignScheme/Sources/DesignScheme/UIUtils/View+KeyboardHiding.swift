ab00b43269143ee94eb72d1ccf9002efd178039c
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2024-10-20 12:18:40 +0300

    fix keyboard dismissal

--- DesignScheme/Sources/DesignScheme/UIUtils/View+KeyboardHiding.swift
+++ DesignScheme/Sources/DesignScheme/UIUtils/View+KeyboardHiding.swift
@@ -9,14 +9,26 @@ import SwiftUI
 
 extension View {
     public func hideKeyboardOnTap() -> some View {
-        onTapGesture {
-            self.hideKeyboard()
-        }
+        onAppear(perform: KeyboardClosingUIGestureRecognizer.connectToKeyWindow)
+    }
+}
+
+final class KeyboardClosingUIGestureRecognizer: UITapGestureRecognizer, UIGestureRecognizerDelegate {
+    static func connectToKeyWindow() {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .first(where: { $0 is UIWindowScene })
+            .flatMap({ $0 as? UIWindowScene })?.windows
+            .first(where: \.isKeyWindow)
+        guard let keyWindow else { return }
+        let tapGesture = KeyboardClosingUIGestureRecognizer(target: keyWindow, action: #selector(UIView.endEditing))
+        tapGesture.requiresExclusiveTouchType = false
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = tapGesture
+        keyWindow.addGestureRecognizer(tapGesture)
     }
     
-    private func hideKeyboard() {
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let window = windowScene.windows.first else { return }
-        window.endEditing(true)
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false
     }
 }
